68020ce8bfde1177393709ad2b2baf8cde6957c1
Lucas Lourenção <lucas.l.lourencao@gmail.com>
2023-02-16 04:20:17 -0300

    Anotações_py_01

---
+++ Python/Basic Python/Variaveis_py/variaveis.py
@@ -0,0 +1,12 @@
+# 1. Atividade com variáveis python:
+
+nome = "Jessika"  
+idade = 28
+altura = 1.68
+e_matematica = True
+
+print(f'{nome} é uma mulher de {idade} anos, {altura}m e gosta de matemática.')
+
+
+# Anotações:
+# na definição da variável sempre o nome  =  valor (o "=" é chamado de operador de atribuição).
