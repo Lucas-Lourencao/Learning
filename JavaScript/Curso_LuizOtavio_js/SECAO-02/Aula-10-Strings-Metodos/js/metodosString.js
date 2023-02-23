2c0f079612790d2df2d3a210d557b3f77f016db7
Lucas Lourenção <lucas.l.lourencao@gmail.com>
2023-02-23 16:45:15 -0300

    C02-task-L.O.M-js

--- JavaScript/Curso_LuizOtavio_js/SECAO-02/Aula-10-Strings-Metodos/js/metodosString.js
+++
@@ -1,7 +0,0 @@
-//1) Método Length
-function tamanhoString() {
-    let verificaTamanho = document.getElementById("metodoLenght");
-    let tamanho = verificaTamanho.length;
-    document.getElementById("divOculta").innerHTML(`<p>A palavra digitada tem ${tamanho} caracteres!</p>`);
-}
-
