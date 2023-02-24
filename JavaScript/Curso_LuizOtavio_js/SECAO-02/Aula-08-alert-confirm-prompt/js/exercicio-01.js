4bbc1b16ade42f68cd125a0c7e41b0b7e175a9d6
Lucas Lourenção <lucas.l.lourencao@gmail.com>
2023-02-24 06:05:23 -0300

    C002-task-clear-branch

--- JavaScript/Curso_LuizOtavio_js/SECAO-02/Aula-08-alert-confirm-prompt/js/exercicio-01.js
+++
@@ -1,7 +0,0 @@
-// Para conversão da string em number, também poderia ter sido usado o parseInt ou parseFloat
-let num1 = Number(prompt('informe um número: '));  
-let num2 = Number(prompt('informe outro número: ')); 
-
-const resultado = num1 + num2;
-
-window.alert(`A soma dos números ${num1} e ${num2} é igual a ${resultado}`);
