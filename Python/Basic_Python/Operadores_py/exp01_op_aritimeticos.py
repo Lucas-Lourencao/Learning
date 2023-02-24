dac65870217fa6ca0c10b8ddd7c73e9930c8bbc7
Lucas Lourenção <lucas.l.lourencao@gmail.com>
2023-02-24 06:03:17 -0300

    C001-task-clear-branch

---
+++ Python/Basic_Python/Operadores_py/exp01_op_aritimeticos.py
@@ -0,0 +1,33 @@
+# 1. Inicializando algumas variáveis:
+a = 2
+b = 5
+d = "para"
+e = "lelepípedo"
+
+# 1.1 Adição numérica:
+print(a + b)
+
+# 1.2 Adição strings:
+print(d)
+print(e)
+print(d+e)
+
+# 1.3 Subtração:
+print(a - b)
+
+# 1.4 Divisão:
+print(b/a)
+
+# 1.5 Parte inteira da divisão:
+print(b//a)
+
+# 1.6 Resto da divisão:
+print(b%a)
+
+# 1.7 Multiplicação:
+print(a*b)
+
+#1.8 Potência:
+print(a**3)
+print(a**2)
+print(a**2**3)
