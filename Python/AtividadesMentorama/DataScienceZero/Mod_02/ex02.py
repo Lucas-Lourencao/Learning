fba7bf460b60187f4051e248d7218a7a4dcf4d69
Lucas Lourenção <lucas.l.lourencao@gmail.com>
2023-02-24 06:07:51 -0300

    C004-task-clear-branch

---
+++ Python/AtividadesMentorama/DataScienceZero/Mod_02/ex02.py
@@ -0,0 +1,14 @@
+# Exercício 2: 
+# Exercício 2.1: 
+listaString = ['arroz', 'feijão', 'macarrão', 'legumes', 'carne', 'ovos', 'macarrão', 'arroz']
+
+# Transformando a lista de Strings em um conjunto de Strngs.
+conjString = set(listaString)
+
+#  Repara que a saída não conterá elementos repetidos.
+print(conjString)
+
+# Exercício 2: 
+# Exercício 2.2:
+print(len(listaString))
+print(len(conjString))
