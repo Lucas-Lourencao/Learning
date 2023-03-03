6aa267e467df2fe1c33091e05973910c5b81466e
Lucas Lourenção <lucas.l.lourencao@gmail.com>
2023-03-03 17:10:14 -0300

    Inclusão das atividades da aula básica de obejtos

---
+++ JavaScript/Curso_LuizOtavio_js/SECAO-02/Aula-15-Objects/objetoSintaxe-01.js
@@ -0,0 +1,12 @@
+// 1) Criando um objeto:
+// 1.1) Estrutura sintática básica:
+const pessoa = {
+    nome:'Lucas',
+    sobrenome:'Lourenção', 
+    idade:35
+};
+
+// 1.2) Acessando dados de um objeto:
+console.log(pessoa.nome);
+console.log(pessoa.sobrenome);
+console.log(pessoa.idade);
