a839bd7aa967ff6207736b4c23f9c2613c2cf2fd
Lucas Lourenção <lucas.l.lourencao@gmail.com>
2023-03-10 16:53:07 -0300

    Atualizações Exercicios

--- JavaScript/Curso_LuizOtavio_js/SECAO-02/Aula-16-Exercicio/js/exercicio-01.js
+++ JavaScript/Curso_LuizOtavio_js/SECAO-02/Aula-16-Exercicio/js/exercicio-01.js
@@ -2,13 +2,17 @@
 function formulario(){
     const form = document.querySelector('.form');
 
+    // contador criado apenas para demonstrar o número de envios
     let contador = 1;
     function recebeEventoForm(evento){
+        // evento chamado para não permitir a atualização da página logo após o submit
         evento.preventDefault();
+
         console.log(`Pela ${contador}ª o form não foi enviado!`);
         contador++;
     };
 
+    // Evento para escutar a ocorrência de evento específico
     form.addEventListener('submit', recebeEventoForm);
 };
 
